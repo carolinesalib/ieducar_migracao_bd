5a30be927dee9cd17bdbc873086447c3b35212bb
Caroline Salib <carolinesalibc@gmail.com>
2019-02-04 18:10:16 -0200

    Ajusta tabela de auditoria

--- scripts/01_normaliza_tabelas/12_modules_auditoria_geral.sql
+++ scripts/01_normaliza_tabelas/12_modules_auditoria_geral.sql
@@ -1,15 +1,14 @@
-create table modules.auditoria_geral
-(
-	usuario_id integer,
-	operacao smallint,
-	rotina varchar(50),
-	valor_antigo json,
-	valor_novo json,
-	data_hora timestamp,
-	codigo varchar
+CREATE TABLE modules.auditoria_geral (
+    usuario_id integer,
+    operacao smallint,
+    rotina character varying(50),
+    valor_antigo json,
+    valor_novo json,
+    data_hora timestamp without time zone,
+    codigo character varying,
+    id integer NOT NULL,
+    query text
 );
-
-alter table modules.auditoria_geral owner to ieducar;
 
 CREATE SEQUENCE modules.auditoria_geral_id_seq
     START WITH 1
